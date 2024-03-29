9a1df1fc19de2dfa7a34361a34874de24b055265
Jack Gardner <jackmcggardner@outlook.com>
2024-03-29 20:47:43 +1100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,167 @@
 // Hero Health
-var heroHealth = 400;
+var heroHealth = 525;
 document.getElementById("heroHealth").textContent = heroHealth;
+
+// Damage per Pellet
+var damagePellet = '8 - 2.4';
+document.getElementById("damagePellet").textContent = damagePellet;
+
+// Damage per Shot
+var damageShot = '80 - 24';
+document.getElementById("damageShot").textContent = damageShot;
+
+// Falloff Range
+var falloffRange = '15 - 25';
+document.getElementById("falloffRange").textContent = falloffRange;
+
+// Headshot
+var headshot = '✔';
+document.getElementById("headshot").textContent = headshot;
+
+// Rate of Fire
+var rateOfFire = 1.33;
+document.getElementById("rateOfFire").textContent = rateOfFire;
+
+// Bullets per Shot
+var bulletsPerShot = 10;
+document.getElementById("bulletsPerShot").textContent = bulletsPerShot;
+
+// Ammo per shot
+var ammoPerShot = 1;
+document.getElementById("ammoPerShot").textContent = ammoPerShot;
+
+// Ammo
+var ammo = 8;
+document.getElementById("ammo").textContent = ammo;
+
+// Reload Time
+var reloadTime = 1.5;
+document.getElementById("reloadTime").textContent = reloadTime;
+
+// Spread Angle
+var spreadAngle = 1.8;
+document.getElementById("spreadAngle").textContent = spreadAngle;
+
+// Projectile Radius
+var projectileRadius = 0.05;
+document.getElementById("projectileRadius").textContent = projectileRadius;
+
+// Blade Damage Hit
+var bladeDamageHit = 65;
+document.getElementById("bladeDamageHit").textContent = bladeDamageHit;
+
+// Thrown Hit Wound
+var thrownHitWound = '30 over 3 seconds';
+document.getElementById("thrownHitWound").textContent = thrownHitWound;
+
+// Return Hit Wound
+var returnHitWound = '15 over 3 seconds';
+document.getElementById("returnHitWound").textContent = returnHitWound;
+
+// Blade Headshot
+var bladeHeadshot = '✘';
+document.getElementById("bladeHeadshot").textContent = bladeHeadshot;
+
+// Cast Time
+var castTime = '0.13 + 0.24';
+document.getElementById("castTime").textContent = castTime;
+
+// Blade Duration
+var bladeDuration = 3;
+document.getElementById("bladeDuration").textContent = bladeDuration;
+
+// Max Range
+var bladeMaxRange = 60;
+document.getElementById("bladeMaxRange").textContent = bladeMaxRange;
+
+// Blade Projectile Speed
+var bladeProjectileSpeed = 40;
+document.getElementById("bladeProjectileSpeed").textContent = bladeProjectileSpeed;
+
+// Blade Projectile Radius
+var bladeProjectileRadius = 0.35;
+document.getElementById("bladeProjectileRadius").textContent = bladeProjectileRadius;
+
+// Shout Self Overhealth
+var selfOverhealth = 150;
+document.getElementById("selfOverhealth").textContent = selfOverhealth;
+
+// Shout Ally Overhealth
+var allyOverhealth = 50;
+document.getElementById("allyOverhealth").textContent = allyOverhealth;
+
+// Shout Cast Time
+var shoutCastTime = 'None';
+document.getElementById("shoutCastTime").textContent = shoutCastTime;
+
+// Shout Self Duration
+var selfDuration = 5;
+document.getElementById("selfDuration").textContent = selfDuration;
+
+// Shout Self Duration
+var allyDuration = 3;
+document.getElementById("allyDuration").textContent = allyDuration;
+
+// Area of Effect
+var areaOfEffect = 15;
+document.getElementById("areaOfEffect").textContent = areaOfEffect;
+
+// Movement Speed
+var movementSpeed = 30;
+document.getElementById("movementSpeed").textContent = movementSpeed;
+
+// Axe Impact Damage
+var axeDamage = 90;
+document.getElementById("axeDamage").textContent = axeDamage;
+
+// Axe Wound Damage
+var axeWoundDamage = '40 over 3';
+document.getElementById("axeWoundDamage").textContent = axeWoundDamage;
+
+// Axe Cast Time
+var axeCastTime = '1 + 0.26';
+document.getElementById("axeCastTime").textContent = axeCastTime;
+
+// Axe Duration
+var axeDuration = 3;
+document.getElementById("axeDuration").textContent = axeDuration;
+
+// Axe Hitbox Active
+var axeHitboxActive = 0.144;
+document.getElementById("axeHitboxActive").textContent = axeHitboxActive;
+
+// Axe Max Range
+var axeMaxRange = 5;
+document.getElementById("axeMaxRange").textContent = axeMaxRange;
+
+// Rampage Damage
+var rampageDamage = '40 impact over 4.5 seconds';
+document.getElementById("rampageDamage").textContent = rampageDamage;
+
+// Rampage Cast Time
+var rampageCastTime = '0.74 + 0 seconds';
+document.getElementById("rampageCastTime").textContent = rampageCastTime;
+
+// Rampage DoT Duration
+var dotDuration = 4.5;
+document.getElementById("dotDuration").textContent = dotDuration;
+
+// Rampage Dash Duration
+var dashDuration = 0.864;
+document.getElementById("dashDuration").textContent = dashDuration;
+
+// Rampage Max Range
+var rampageMaxRange = 25;
+document.getElementById("rampageMaxRange").textContent = rampageMaxRange;
+
+// Rampage Area of Effect
+var rampageAreaOfEffect = 5;
+document.getElementById("rampageAreaOfEffect").textContent = rampageAreaOfEffect;
+
+// Rampage Movement Speed
+var rampageMovementSpeed = 33;
+document.getElementById("rampageMovementSpeed").textContent = rampageMovementSpeed;
+
+// Adrenaline Rush Healing
+var adrenalineRushHealing = 200;
+document.getElementById("adrenalineRushHealing").textContent = adrenalineRushHealing;
